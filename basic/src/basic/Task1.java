01b793cdf72b9fd8d1da27ef25d7421b56f0d06c
airmokugyo <koreore.jp@gmail.com>
2020-08-18 01:24:40 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task1.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("初めての課題");
+		System.out.print("Java攻略の道");
+	}
+
+}
---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Task2 {
+
+   	public static void main(String[] args) {
+		// コメント
+		// コメント
+		System.out.println("hogehoge");
+		System.out.println("hogehoge2");
+
+	}
+
+}
