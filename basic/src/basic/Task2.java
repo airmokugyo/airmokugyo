fed3e863ca431dbe928f49a13d87a62669484c5c
airmokugyo <koreore.jp@gmail.com>
2020-08-18 21:37:43 +0900

    再提出 - Javaの基礎

--- basic/src/basic/Task2.java
+++ basic/src/basic/Task2.java
@@ -3,8 +3,8 @@ package basic;
 public class Task2 {
 
    	public static void main(String[] args) {
-		// コメント
-		// コメント
+		/* コメント
+		   コメント */
 		System.out.println("hogehoge");
 		System.out.println("hogehoge2");
 
